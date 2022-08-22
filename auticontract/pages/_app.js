bb659fadaa653d95a99ea2f515aa3d57bc587723
Felix Schlegel <schlegel.felix@gmail.com>
2022-08-22 12:42:01 +0000

    added navbar header using web3uikit and tailwind

--- auticontract/pages/_app.js
+++ auticontract/pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Header from "../components/Header"
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>AuticContract</title>
+                <meta name="description" content="Auticontract web app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
